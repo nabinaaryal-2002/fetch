699b8c1ecc2cf321b024048c9edb26c66bc6336c
Nabina aryal <106301164+nabinaaryal-2002@users.noreply.github.com>
2023-10-04 16:16:40 +0545

    changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Get Request 
+
 function FetchTodo(){
   fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(function(data){
@@ -14,3 +16,40 @@ function FetchTodo(){
   })
 
 }
+
+
+// Another Get Request 
+
+console.log("Hello, Good Morning");
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(function(data){
+  return data.json();
+}).then(function(data){
+  console.log(data);
+})
+
+
+// Post Method 
+
+
+const res = {
+  title: "This is title",
+  body: "This is post body",
+  userId: 2
+}
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify(res),
+  headers: {
+    "Content-type": "application/json"
+
+  }
+  
+}).then(function(data){
+  return data.json();
+}).then(function(res){
+  console.log(res);
+})
